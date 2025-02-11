8a32095884df07a8598d39d972c9192510562c0d
TZ-Boomer <julienmarty0819@gmail.com>
2025-02-11 17:48:04 +0100

    Link between scenes

---
+++ Assets/Scripts/Game/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public float delayBeforeNextScene = 5.0f; // Time before switching automatically
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("SnowMountains JULIEN"); // Ensure this matches the actual scene name
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("No more scenes to load!");
+        }
+    }
+
+    public void AutoLoadNextScene()
+    {
+        Invoke("LoadNextScene", delayBeforeNextScene); // Only called when needed
+    }
+}
